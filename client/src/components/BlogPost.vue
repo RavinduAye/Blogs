d0b34a80f929cc472d5f5791df71f2b68bd17c10
RavinduAye <ravinduamaya@gmail.com>
2023-10-20 14:01:03 +0530

    create and update post added

--- client/src/components/BlogPost.vue
+++ client/src/components/BlogPost.vue
@@ -1,78 +1,120 @@
 <template>
-    <div class="blog-container">
-        <div class="blog-image-container col-md-12">
-            <img class="img-fluid" :src="blog.image">
-        </div>
-        <div class="blog-title col-md-12"> {{ blog.title }}</div>
-        <div class="blog-content-container col-md-12">
-            <div class="blog-content"> {{ blog.content }}</div>
-        </div>
-        <div class="col-md-12">
-            <button type="button">
-                <v-icon>plus</v-icon>
-            </button>
-            <button type="button">
-                <v-icon>delete</v-icon>
-            </button>
-        </div>
+  <div class="field-container">
+    <div v-if="!isEdit">
+      Create New
     </div>
+    <div class="mt-5">
+      <v-text-field
+        v-model="model.title"
+        label="Title"
+        maxlength="150"
+        counter
+        :rules="[rules.required, rules.titleCounter]"
+        clearable
+      ></v-text-field>
+    </div>
+    <div class="col-md-12">
+      <v-textarea
+        v-model="model.content"
+        label="Content"
+        maxlength="1000"
+        counter
+        :rules="[rules.required, rules.contentCounter]"
+        clearable
+      ></v-textarea>
+    </div>
+    <div class="button-container">
+      <div v-if="isEdit">
+        <v-btn  class="btn" color="blue" :disabled="!model.title || !model.content">Save</v-btn>
+        <v-btn class="btn" color="#ff6347" @click="cancel">Cancel</v-btn>
+      </div>
+      <div v-else>
+        <v-btn  class="btn" color="blue" :disabled="!model.title || !model.content">Publish</v-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    title: 'BlogPost',
-    data: () => ({
-        blog:
-            { 
-                image: require(`../assets/logo.png`),
-                title: 'Test Blog',
-                content: 'Test Blog Content'
-            },
-        cart: true,
-    }),
+  title: "BlogPost",
+  data: () => ({
+    blog: {
+      image: require(`../assets/blogImage4.jpg`),
+    },
+    loading: false,
+    model: {
+      title: '',
+      content: ''
+    },
+    rules: {
+      required: (value) => !!value || "This field is Required.",
+      titleCounter: (value) => value.length <= 150 || "Max 150 characters",
+      contentCounter: (value) => value.length <= 1000 || "Max 1000 characters",
+    },
+  }),
 
-    methods : {
-        
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    isEdit:{
+      type: Boolean,
+      default: false,
     }
-}
+  },
+
+  mounted() {
+    this.model.title = this.title;
+    this.model.content = this.content;
+  },
+
+  methods: {
+    save(){
+        const data = {
+            title : this.model.title,
+            content : this.model.content
+        }
+
+        console.log(data);
+    },
+
+    cancel(){
+      this.$emit('edit-post', false);
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 
-.blog-container {
-    transition: box-shadow .3s;
-    background-color: #fff;
-    border-radius: 1rem;
-    padding: 0.625rem;
-    text-align: center;
-    letter-spacing: .01rem;
-    font-size: 1rem;
-    // width: min-content;
+.field-container {
+  transition: box-shadow 0.3s;
+  background-color: #fff;
+  border-radius: 1rem;
+  padding: 0.625rem;
+  text-align: left;
+  letter-spacing: 0.01rem;
+  font-size: 1rem;
+  margin-top: 5%;
+  // width: 70%;
 }
 
-.blog-image-container {
-    position: relative;
-    display: inline-block;
-    cursor: pointer;
+.button-container {
+  margin: 50px 0px 10px 0px;
+  display: flex;
+  justify-content: right;
 }
 
-.blog-title {
-    color: #023047;
-    text-align: center;
-    text-overflow: ellipsis;
-    font-family: cursive;
-    padding: 0px 15px;
+.btn{
+    margin-left: 20px;
+    color: #fff;
 }
 
-.blog-content {
-    color: #219ebc;
-    font-family: cursive;
-    font-weight: bold;
-}
-
-.blog-container:hover {
-    box-shadow: 0.625rem 0.625rem 0.6875rem rgba(142, 202, 230, 0.0588235294);
-}
 
 </style>
