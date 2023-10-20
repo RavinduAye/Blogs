2029949083dbd8dcb20696627ef03d918c266fc6
RavinduAye <ravinduamaya@gmail.com>
2023-10-20 14:01:02 +0530

    carousel added

---
+++ client/src/components/CarouselSlide.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-carousel
+      :show-arrows="false"
+      :show-indicators="false"
+      hide-delimiters
+      cycle
+    >
+      <div>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </div>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  title: "CarouselSlide",
+  data: () => ({
+    items: [
+      {
+        src: require(`../assets/blogImage2.jpg`),
+      },
+      {
+        src: require(`../assets/blogImage3.jpg`),
+      },
+    ],
+  }),
+
+  methods: {},
+};
+</script>
